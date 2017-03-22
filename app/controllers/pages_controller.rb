705f011cf16499609ddf17fefe061bc69340a290
olegsugatov <oleg.sugatov@gmail.com>
2017-03-22 17:43:46 +0300

    bootstap init

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+	
+	def welcome
+		@title = "Welcome!"
+	end
+
+	def about_us
+		@title = "We a learning people programming and make a world better!"
+	end
+
+end
